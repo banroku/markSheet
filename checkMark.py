e29dfcdd973ce5d2d29ad027556e65282249c03b
banroku <sakai.fuji.jpn@gmail.com>
2018-05-06 05:15:05 +0900

    commit

--- checkMark.py
+++ checkMark.py
@@ -2,13 +2,35 @@
 
 import numpy as np
 import cv2
+from pdf2image import convert_from_path
 
 landmark = np.zeros((3,2))
 
-img = cv2.imread('sample-image.png', 0)
+#pdfを読み込み、opevCVで使えるarrayに変換
+imgAll = convert_from_path('scanImage/test1.pdf', dpi=100)
+img = imgAll[0]    #pick first page up
+img = np.asarray(img)
+img = img.take([1,2,0], axis=2) #channel変換(GBR -> RGB)
+
+#幅が1084からずれていた場合、拡大or縮小する
+if img.shape[1] != 1084:
+    width = 1084
+    height = int(round(img.shape[0] / img.shape[1] * 1084))
+    img = cv2.resize(img, (width, height), interpolation = cv2.INTER_CUBIC)
+
+#グレイスケールに変換
+img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+#marker画像の登録
 marker= cv2.imread('landmark.png', 0)
-markerCenter = [20, 21]
+markerCenter = np.array([20, 21])
+
 result = cv2.matchTemplate(img, marker, 0)
+result = (1-result/np.max(result))*255
+M = np.float32([
+[1, 0, markerCenter[1]] ,
+[0, 1, markerCenter[0]] ])
+result = cv2.warpAffine(result, M, (width, height))
 
 answer = [
 ['molding', 668, 115, 0],
@@ -22,34 +44,46 @@ answer = [
 ['others', 708, 241, 0],
 ]
 
-#! for でanswerの座標から、markerCenterを引き算する。
+guide = np.array([
+[726, 31],
+[726, 1051],
+[29, 31], ])
+
+#landmark座標の読み取り
+scope = 25  #本来のguide点からのズレの許容範囲
+mask = np.zeros(result.shape)
+
+for i in range(0, landmark.shape[0]):
+    mask[:] = 0
+    mask_xfr = max(0, guide[i,0]-(scope+markerCenter[0]))
+    mask_xto = min(width, guide[i,0]+(scope+markerCenter[0]))
+    mask_yfr = max(0, guide[i,1]-(scope+markerCenter[1]))
+    mask_yto = min(width, guide[i,1]+(scope+markerCenter[1]))
+    mask[mask_xfr:mask_xto, mask_yfr:mask_yto] = 255
+    min_val, max_val, min_val, landmark[i,:] = cv2.minMaxLoc(np.multiply(result, mask))
+
+landmark = np.take(landmark, [1,0], axis=1) #x,yが逆転しているのを直す
 
 
+#shift
+shift = landmark 
 
-#位置決め
-maskAll = np.ones(result.shape) * np.max(result)
-mask = maskAll[:]
-mask[:49, :49] = 0
-resultMasked = result + mask
-min_val, max_val, landmark[0,:], max_loc = cv2.minMaxLoc(resultMasked)
-
-mask = maskAll[:]
-mask[-49:, :49] = 0
-resultMasked = result + mask
-min_val, max_val, landmark[1,:], max_loc = cv2.minMaxLoc(resultMasked)
-
-mask = maskAll[:]
-mask[-49:, -49:] = 0
-resultMasked = result + mask
-min_val, max_val, landmark[2,:], max_loc = cv2.minMaxLoc(resultMasked)
+#scale & rotate
+center = landmark[1,:]
+radius = np.linalg.norm(landmark[2,:] - landmark[1,:])
+scale = radius/np.linalg.norm(landmark0[2,:] - landmark0[1,:])
+cos = (landmark[2,0]-landmark[1,0])/radius
+sin = (landmark[2,1]-landmark[1,1])/radius
+alpha = scale * cos
+beta = scale * sin
+M = np.array([
+[alpha, beta, (1-alpha) * center[1] - beta * center[0]],
+[- beta, alpha, (benta * center[1] - (1-alpha) * center[0]
+])
+dst = cv2.warpAffine(result,M,(1084,750))
 
 
-#calculate origin
 #calculate slope of x, y
-
-#define coordinates of answer marks (dictで作る, これが面倒くさい。
-#あるいは、全部黒塗りにされているシートから自動で読み込む。
-#そもそもマークシートの形状は円形いったくだから形状の工夫の余地はない
 
 #convert coordinates of answer marks along origin/slopex, slope y
 
